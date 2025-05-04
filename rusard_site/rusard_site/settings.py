1d19a8b30f0e901cbf5fad4c71cb7c731c93a391
Rusard <rusard.diy@gmail.com>
2025-05-04 10:44:17 +0200

    nouveasu test de modification de paramètre

--- rusard_site/rusard_site/settings.py
+++ rusard_site/rusard_site/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'rusard.ch', 'www.rusard.ch']
-
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "").split(",")
 
 # Application definition
 
