8668036dccd1d5db995f0c7603bb927fe0dd1638
Rusard <rusard.diy@gmail.com>
2025-05-22 06:06:37 +0200

    tentative de site map

---
+++ rusard_site/rusardhome/sitemaps.py
@@ -0,0 +1,25 @@
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+from .models import Article  # ou un autre modèle de ton site
+
+
+class StaticViewSitemap(Sitemap):
+    priority = 0.5
+    changefreq = 'monthly'
+
+    def items(self):
+        return ['accueil', 'modelisation', 'contact']  # noms des vues `name=` dans urls.py
+
+    def location(self, item):
+        return reverse(item)
+
+
+class ArticleSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.8
+
+    def items(self):
+        return Article.objects.all()
+
+    def lastmod(self, obj):
+        return obj.updated_at  # assure-toi que ton modèle a un champ `updated_at`
